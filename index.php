62fc2eabc634793edbdc37a9bee5cafa51512c51
Sama Bomadian <samabomadian7@gmail.com>
2026-07-24 22:23:47 +0300

    Initial project structure

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include "includes/header.php"; ?>
+
+<?php include "includes/navbar.php"; ?>
+
+<div class="container mt-5">
+    <h1>Home Page</h1>
+</div>
+
+<?php include "includes/footer.php"; ?>
