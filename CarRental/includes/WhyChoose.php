cd20cd057ce496bf150a19036e908a154a32367a
shahdadelspace0123-cmyk <shahdadelspace0123@gmail.com>
2026-07-26 02:42:02 +0300

    update CarRental project

---
+++ CarRental/includes/WhyChoose.php
@@ -0,0 +1,59 @@
+<section class="container py-5">
+
+    <div class="row align-items-center">
+
+        <div class="col-lg-6">
+            <img src="includes/images/car.jpeg" class="img-fluid" alt="Car">
+        </div>
+
+        <div class="col-lg-6">
+
+            <span class="badge bg-light text-primary mb-3">
+                WHY CHOOSE US
+            </span>
+
+            <h2 class="fw-bold mb-4">
+                We offer the best experience with our rental deals
+            </h2>
+
+            <div class="d-flex mb-4">
+                <i class="bi bi-wallet2 text-primary fs-3 me-3"></i>
+
+                <div>
+                    <h5>Best price guaranteed</h5>
+                    <p>Find a lower price? We'll refund you 100% of the difference.</p>
+                </div>
+            </div>
+
+            <div class="d-flex mb-4">
+                <i class="bi bi-person-check text-primary fs-3 me-3"></i>
+
+                <div>
+                    <h5>Experience driver</h5>
+                    <p>Don't have driver? Don't worry, we have many experienced drivers for you.</p>
+                </div>
+            </div>
+
+            <div class="d-flex mb-4">
+                <i class="bi bi-truck text-primary fs-3 me-3"></i>
+
+                <div>
+                    <h5>24 hour car delivery</h5>
+                    <p>Book your car anytime and we will deliver it directly to you.</p>
+                </div>
+            </div>
+
+            <div class="d-flex">
+                <i class="bi bi-headset text-primary fs-3 me-3"></i>
+
+                <div>
+                    <h5>24/7 technical support</h5>
+                    <p>Have a question? Contact us anytime.</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
