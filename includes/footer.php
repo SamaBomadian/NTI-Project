c357fe6dc5e46c4c6a4a3812e35380e39f6fe719
an632430-web <an632430@gmail.com>
2026-07-25 23:39:46 +0300

    second layout-update

--- includes/footer.php
+++ includes/footer.php
@@ -1,58 +1,63 @@
 
-    <!-- <footer class="px-3"> 
-        <div class="row"> 
-            <div class="col-6 col-md-2 mb-3"> 
-                <h5>Section</h5> 
-                <ul class="nav flex-column"> 
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li> 
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li> 
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li> 
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li> 
-                    <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li> 
-                </ul> </div>
-                 <div class="col-6 col-md-2 mb-3"> 
-                    <h5>Section</h5> 
+<footer class="px-5 " style="background-color:#051C34;color:white;"> 
+        <div class="row pt-3"> 
+            <div class="col-6 col-md-3 d-flex flex-column " > 
+                <img src="assets/image/Frame 993 (2).png" style="width:100px;height:30px;margin-bottom:5px"/> 
+                <ul class="nav flex-column mt-2" > 
+                    <li class="nav-item my-2 d-flex align-items-start align-content-center gap-2"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+</svg><P href="#" class="nav-link p-0 "> 25566 Hc1,Cairo,12345,Egypt</P></li> 
+                    <li class="nav-item mb-2 d-flex align-items-start align-content-center gap-2"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="white" class="bi bi-telephone" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg><p href="#" class="nav-link p-0 ">+20 1016547660</p></li> 
+                    <li class="nav-item mb-2 d-flex align-items-start align-content-center gap-2"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="White" class="bi bi-envelope" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+</svg><p href="#" class="nav-link p-0 ">Abdo@test.com</p></li> 
+                    
+                </ul> 
+            </div>
+                 <div class="col-6 col-md-3 "> 
+                    <h5>Our Product</h5> 
                     <ul class="nav flex-column"> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Career</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Car</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Packages</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">Features</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Priceline</a></li> 
                     </ul> 
                 </div> 
-                <div class="col-6 col-md-2 mb-3"> 
-                    <h5>Section</h5> 
+                <div class="col-6 col-md-3 "> 
+                    <h5>Resources</h5> 
                     <ul class="nav flex-column"> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Home</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Features</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Pricing</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">FAQs</a></li> 
-                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Download</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Help Center</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Guides</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Partner Network</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Cruises</a></li>
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Developer</a></li>
                      </ul> 
                     </div> 
-                    <div class="col-md-5 offset-md-1 mb-3"> 
-                        <form>
-                             <h5>Subscribe to our newsletter</h5> 
-                             <p>Monthly digest of what's new and exciting from us.</p> 
-                             <div class="d-flex flex-column flex-sm-row w-100 gap-2"> 
-                                <label for="newsletter1" class="visually-hidden">Email address</label> <input id="newsletter1" type="email" class="form-control" placeholder="Email address"> 
-                                <button class="btn btn-primary" type="button">Subscribe</button> </div> </form> </div> </div> 
-                                <div class="d-flex flex-column flex-sm-row justify-content-center pt-4 mt-4 border-top"> 
+                    <div class="col-6 col-md-3 "> 
+                    <h5>About Rentcars</h5> 
+                    <ul class="nav flex-column"> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Why Choose Us</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Our Story</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">investor Relations</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Press Center</a></li> 
+                        <li class="nav-item mb-2"><a href="#" class="nav-link p-0">Advertise</a></li>
+                        
+                     </ul> 
+                    </div>  
+                                <div class="d-flex flex-column flex-sm-row  pt-4 mt-4 px-3 border-top align-content-between justify-content-between"> 
                                     <p>© 2025 Company, Inc. All rights reserved.</p> 
+                                    <ul class="flex-row nav justify-content-between align-content-center gap-4 mb-3">
+                                        <li><a href="#" class="nav-link p-0"><img src="assets/image/facebook.png" style="width:25px;height:25px"/></a></li>
+                                        <li><a href="#" class="nav-link p-0"><img src="assets/image/twitter (1).png" style="width:25px;height:25px"/></a></li>
+                                        <li><a href="#" class="nav-link p-0"><img src="assets/image/instagram (1).png" style="width:25px;height:25px"/></a></li>
+                                    </ul>
                                     
                                  </div>
-                                 </footer> -->
-<footer class="d-flex flex-wrap justify-content-between align-items-center pt-3 my-4 px-3 border-top"> 
-    <p class="col-md-4 mb-0 text-body-secondary">© 2025 Company,Inc. All Rights Reserved</p> 
-    <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none" aria-label="Bootstrap">
-         <img style="width:80px;height:20px" src="assets/image/logo-black.png" href="logo"/>
-    </a> 
-    <ul class="nav col-md-4 justify-content-end"> 
-        <li class="nav-item"><a href="index.php" class="nav-link px-2 text-body-secondary">Home</a></li> 
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li> 
-        <li class="nav-item"><a href="contact.php" class="nav-link px-2 text-body-secondary">contact US</a></li> 
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li> 
-        <li class="nav-item"><a href="about.php" class="nav-link px-2 text-body-secondary">About</a></li> 
-    </ul> 
 </footer>
+
     
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,14 +1,14 @@
 
-<nav class="navbar navbar-expand-lg fixed-top" style="background-color: #e3f2fd;" data-bs-theme="light">
-  <div class="container-fluid">
+<nav class="navbar navbar-expand-lg fixed-top px-5" style="background-color: transparent;" data-bs-theme="light">
+  <div class="container-fluid d-flex justify-content-between">
     <a class="navbar-brand" href="index.php">
-        <img  src="assets/image/logo-black.png"/>
+        <img  src="assets/image/Frame 993.png"/>
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0 gap-3">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         </li>
@@ -21,11 +21,12 @@
        
         
       </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-primary" type="submit">Search</button>
-      </form>
+      <div class="d-flex gap-3">
+        <a href="login.php" class="btn">sign In</a>
+        <a href="login.php" class="btn px-4" style="background-color:#1572D3;color:white;">sign Up</a>
     </div>
+    </div>
+    
   </div>
 </nav>
     
